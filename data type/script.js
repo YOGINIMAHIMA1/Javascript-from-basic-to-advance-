77ce000ab77fc5fe31c24d714dd9c8497892a61b
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-11-13 01:59:49 +0530

    Create script.js

---
+++ data type/script.js
@@ -0,0 +1,8 @@
+// primitive data type
+// string, number, boolean, null,undefined, symbol,BigInt
+//refrence data type
+//object literals, function,arrays
+
+//----  strings---
+ let str='mahima';
+console.log(str);//mahima
