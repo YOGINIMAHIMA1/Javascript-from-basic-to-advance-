429570b495fecb346f4b5241e1541faeb10e21cd
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-12-06 21:18:06 +0530

    Create script.js

---
+++ Console/script.js
@@ -0,0 +1,26 @@
+//log number
+console.log(45);
+//log string 
+console.log('Hi , How are You?');
+//log Multiple values
+console.log(30, 'Hello World');
+// log a variable
+let a=10;
+console.log(a);
+//log Alert and warning
+console.log('alert');
+console.warn('warning');
+
+// log object as table
+console.table({name:'Brad',email:'brad@gmail.com'});
+//ctrl+l ---to clear the console
+//log group in console
+console.group('data');
+console.log(10);
+console.warn('warning');
+console.error('error');
+console.groupEnd();
+// add css to log
+const x='padding:20px; color:red; background-color:pink;font-size:34px';
+console.log('%c Mahima Bhardwaj',x);
+             
