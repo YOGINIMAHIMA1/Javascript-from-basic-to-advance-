8d603a2f23ae28280d42d27cd401334f5249574a
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-12-23 22:02:38 +0530

    Create string.js

---
+++ Strings/string.js
@@ -0,0 +1,9 @@
+var strName='MotherTeressa';
+console.log(strName);
+// string !== array  i.e string is not an array
+console.log(strName.pop());//it will show an error as pop() cannot be performe don string as string is not an array
+// string concatenation
+var name='Lisa'
+var sirName='Sharma'
+console.log(name+ sirName);
+console.log(name.concat(sirName))
