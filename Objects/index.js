18b3a406a3e15ed60bc4316279d23fe704a20767
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-12-24 02:30:37 +0530

    Create index.js

---
+++ Objects/index.js
@@ -0,0 +1,25 @@
+/*Task:
+A car rental company has asked you to help them organise 
+their fleet of vehicles.
+
+1. Create a new object literal and assign it to the 
+   variable 'hireCar'.
+   
+2. Using the dot notation, assign a 'color' property to 
+   the hireCar object. Its value should be a string 
+   holding a color of your choice.
+   
+3. Using the dot notation, assign an 'isConvertible' property 
+   to the object and give it a boolean value (true/false) 
+   of your choice.
+   
+4. Use the brackets notation to add a 'number of doors' property.
+
+5. Log out the entire object.
+
+*/
+var hireCar={}
+hireCar.color='red'
+hireCar.isConvertible=true
+hireCar['no of doors']=4
+console.log(hireCar)
