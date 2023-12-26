dae0eec14ad55dfd9b62f7d947c466880c8aef32
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-12-26 19:38:19 +0530

    Create ObjectasParameterinFunction.js
    In this code I passed Object as parameter in Function object and inside function if you change any key -value pairf of object it will reflect globally.

---
+++ Objects/ObjectasParameterinFunction.js
@@ -0,0 +1,12 @@
+function Object(obj)
+{
+  obj.color='blue';
+    }
+var myCar= {}
+ car.name='nano;;
+   car.color= 'red';
+car.weight= '230kg';
+console.log(car.color);
+Object(myCar);
+console.log(car.color);
+
