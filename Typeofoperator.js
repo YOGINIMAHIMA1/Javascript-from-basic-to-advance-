e513d726c39c7eb5f98764613244b6211bb9d07d
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-12-26 17:11:19 +0530

    Update Typeofoperator.js

--- Typeofoperator.js
+++ Typeofoperator.js
@@ -1,1 +1,14 @@
-
+typeof('My name is xoxo');// string
+typeof(10);// number
+typeof(10.22);// number
+typeof(true);// boolean
+typeof(false);// boolean
+typeof(10>2);// boolean
+typeof([10,20,30]);// object
+typeof(function(){ console.log('this is function')});//function
+typeof( {
+  obj.name='xoxo',
+   obj.age=20;
+}) //object
+  
+  
