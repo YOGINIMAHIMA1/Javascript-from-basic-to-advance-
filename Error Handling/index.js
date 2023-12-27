21458bd1fe5e179335ea7fc73bd7aeafd4732c98
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-12-27 21:14:48 +0530

    Create index.js

---
+++ Error Handling/index.js
@@ -0,0 +1,32 @@
+//syntax error
+object..create()
+// refrence error as we have not declared variable with any var, const ,let
+"use strict";
+variable=22;
+// type error
+"use strict"
+const name="kylie" // type error
+name="kendal"
+// try catch block
+try
+{
+  const name="xoxo"                      
+{
+  const name="xoxo"
+  name="jiangly"
+}
+catch(err)
+{
+  console.log(err);  //TypeError: Assignment to constant variable.
+                                    //at <anonymous>:4:7
+}    
+// console.error in try catch block
+try
+{
+  const  name="xoxo"
+  name="jiangly"
+}
+catch(err)
+{
+  console.error(err);
+}
